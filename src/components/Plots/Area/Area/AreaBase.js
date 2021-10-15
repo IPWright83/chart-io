445f4b094b68d3499b90b2ec94242901ab499ac0
Ian Wright <ipwright83+dev@gmail.com>
2021-10-15 11:56:13 +0100

    Simplified Area plots

---
+++ src/components/Plots/Area/Area/AreaBase.js
@@ -0,0 +1,98 @@
+import "./Area.css";
+
+import * as d3 from "d3";
+import { useDispatch, useSelector } from "react-redux";
+import PropTypes from "prop-types";
+
+import { useRender } from "../../../../hooks";
+import { chartSelectors, eventSelectors } from "../../../../store";
+import { plotDefaultProps, plotPropTypes } from "../../../../types";
+import { interpolateMultiPath, isNullOrUndefined } from "../../../../utils";
+
+import { useDatumFocus } from "./useDatumFocus";
+import { usePathCreator } from "./usePathCreator";
+
+/**
+ * Represents an Area Plot on an SVG Element
+ * @param  {Object} props       The set of React properties
+ * @return {ReactElement}  The Line plot component
+ */
+const AreaBase = ({ x, y, y2, color, layer, canvas }) => {
+    const dispatch = useDispatch();
+    const data = useSelector((s) => chartSelectors.data(s));
+    const xScale = useSelector((s) => chartSelectors.scales.getScale(s, x));
+    const yScale = useSelector((s) => chartSelectors.scales.getScale(s, y));
+    const eventMode = useSelector((s) => eventSelectors.mode(s));
+    const position = useSelector((s) => eventSelectors.position(s));
+    const theme = useSelector((s) => chartSelectors.theme(s));
+    const width = useSelector((s) => chartSelectors.dimensions.width(s));
+    const height = useSelector((s) => chartSelectors.dimensions.height(s));
+    const animationDuration = useSelector((s) => chartSelectors.animationDuration(s));
+    const sortedData = data.sort((a, b) => d3.ascending(a[x], b[x]));
+
+    const fillColor = d3.color(color || theme.colors[0]);
+    fillColor.opacity = 0.8;
+    const strokeColor = fillColor.darker();
+
+    // Used to create our initial path
+    usePathCreator(layer, x, y, xScale, yScale);
+
+    /* On future renders we want to update the path */
+    useRender(() => {
+        // Area renderer that starts at the 0 point
+        const area = d3
+            .area()
+            .curve(d3.curveLinear)
+            .x((d) => xScale(d[x]))
+            .y0((d) => (y2 ? yScale(d[y]) : yScale.range()[0]))
+            .y1((d) => (y2 ? yScale(d[y] + d[y2]) : yScale(d[y])))
+            .defined((d) => !isNullOrUndefined(d[y]));
+
+        // Handle Canvas rendering
+        if (canvas) {
+            const context = canvas.getContext("2d");
+            area.context(context);
+            // Clear and then re-render the path
+            context.clearRect(0, 0, width, height);
+            context.beginPath();
+            area(sortedData);
+            context.fillStyle = fillColor;
+            context.strokeStyle = strokeColor;
+            context.fill();
+            context.stroke();
+            return;
+        }
+
+        // Handle SVG Rendering
+        d3.select(layer.current)
+            .select("path")
+            .datum(sortedData)
+            .style("fill", fillColor)
+            .style("stroke", strokeColor)
+            .transition("area")
+            .duration(animationDuration)
+            .attrTween("d", function (d) {
+                const previous = d3.select(this).attr("d");
+                const current = area(d);
+                return interpolateMultiPath(previous, current);
+            });
+    }, [x, y, sortedData, xScale, yScale, layer, canvas, width, height, theme.colors, animationDuration]);
+
+    // If possible respond to global mouse events for tooltips etc
+    useDatumFocus(dispatch, layer, x, y, xScale, yScale, sortedData, eventMode, position, strokeColor);
+
+    return null;
+};
+
+AreaBase.propTypes = {
+    ...plotPropTypes,
+    /**
+     * The canvas element that the line chart should render to
+     * @type {Object}
+     */
+    canvas: PropTypes.object,
+};
+
+AreaBase.defaultProps = plotDefaultProps;
+
+export { AreaBase };
