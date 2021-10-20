84eba95c53024c8634526f29fd63417a5fcc1559
Ian Wright <ipwright83+dev@gmail.com>
2021-10-20 14:32:45 +0100

    Switched to JSX stories

---
+++ src/components/Plots/Area/Area.stories.jsx
@@ -0,0 +1,132 @@
+import React from "react";
+
+import { argTypes } from "../../../../stories/argTypes";
+import { sales_records_dataset } from "../../../../data/sales_records_dataset";
+import { Area } from "./Area";
+import { Areas } from "./Areas";
+import { Chart } from "../../Chart";
+import { XAxis, YAxis } from "../../Axis";
+
+import mdx from "./Area.mdx";
+
+const { width, height, margin, useCanvas, theme, color } = argTypes;
+
+export default {
+    title: "XYCharts/Area",
+    component: Area,
+    parameters: {
+        docs: {
+            page: mdx,
+        },
+        chromatic: { delay: 300 },
+    },
+    argTypes: {
+        useCanvas,
+        width,
+        height,
+        theme,
+        color,
+        leftMargin: margin,
+        rightMargin: margin,
+        topMargin: margin,
+        bottomMargin: margin,
+    },
+};
+
+const AreaTemplate = (args) => (
+    <Chart
+        data={sales_records_dataset}
+        margin={{ left: args.leftMargin, right: args.rightMargin, top: args.topMargin, bottom: args.bottomMargin }}
+        ys={[args.y, args.y2, args.y3]}
+        xs={[args.x]}
+        {...args}
+    >
+        <Area x={args.x} y={args.y} y2={args.y2} color={args.color} animationDuration={args.animationDuration} />
+        <YAxis fields={[args.y, args.y2, args.y3]} />
+        <XAxis fields={[args.x]} />
+    </Chart>
+);
+
+const AreasTemplate = (args) => (
+    <Chart
+        margin={{ left: args.leftMargin, right: args.rightMargin, top: args.topMargin, bottom: args.bottomMargin }}
+        data={sales_records_dataset}
+        {...args}
+    >
+        <YAxis fields={[args.y, args.y2, args.y3]} />
+        <XAxis fields={[args.x]} />
+        <Areas x={args.x} ys={[args.y, args.y2, args.y3]} animationDuration={args.animationDuration} />
+    </Chart>
+);
+
+const StackedAreasTemplate = (args) => (
+    <Chart
+        margin={{ left: args.leftMargin, right: args.rightMargin, top: args.topMargin, bottom: args.bottomMargin }}
+        data={sales_records_dataset}
+        {...args}
+    >
+        <YAxis fields={[args.y, args.y2, args.y3]} aggregate={true} />
+        <XAxis fields={[args.x]} />
+        <Areas x={args.x} ys={[args.y, args.y2, args.y3]} stacked={true} animationDuration={args.animationDuration} />
+    </Chart>
+);
+
+export const Basic = AreaTemplate.bind({});
+Basic.storyName = "Basic Plot";
+Basic.args = {
+    useCanvas: false,
+    width: 500,
+    height: 400,
+    animationDuration: 250,
+    color: "#99C1DC",
+    theme: "light",
+    leftMargin: 70,
+    rightMargin: 40,
+    topMargin: 40,
+    bottomMargin: 40,
+    onClick: console.debug,
+    onMouseOver: console.debug,
+    onMouseOut: console.debug,
+    y: "Total Profit",
+    x: "Order Date",
+    y2: undefined,
+};
+
+export const Color = AreaTemplate.bind({});
+Color.storyName = "Custom Color";
+Color.args = {
+    ...Basic.args,
+    color: "orange",
+};
+
+export const Stream = AreaTemplate.bind({});
+Stream.storyName = "Stream Graph";
+Stream.args = {
+    ...Basic.args,
+    y2: "Total Cost",
+};
+
+export const Canvas = AreaTemplate.bind({});
+Canvas.storyName = "Using Canvas";
+Canvas.args = {
+    ...Basic.args,
+    useCanvas: true,
+};
+
+export const MultipleAreas = AreasTemplate.bind({});
+MultipleAreas.storyName = "Mutiple Area Plots";
+MultipleAreas.args = {
+    ...Basic.args,
+    y: "Total Revenue",
+    y2: "Total Cost",
+    y3: "Total Profit",
+};
+
+export const StackedAreas = StackedAreasTemplate.bind({});
+StackedAreas.storyName = "Staced Area Plots";
+StackedAreas.args = {
+    ...Basic.args,
+    y: "Total Revenue",
+    y2: "Total Cost",
+    y3: "Total Profit",
+};
---
+++ src/components/Plots/Bar/Bar.stories.jsx
@@ -0,0 +1,129 @@
+import React from "react";
+
+import { argTypes } from "../../../../stories/argTypes";
+import { sales_records_dataset } from "../../../../data/sales_records_dataset";
+import { Bar } from "./Bar";
+import { Bars } from "./Bars";
+import { Chart } from "../../Chart";
+import { XAxis, YAxis } from "../../Axis";
+
+import mdx from "./Bar.mdx";
+
+const { width, height, margin, useCanvas, theme, color } = argTypes;
+
+export default {
+    title: "XYCharts/Bar",
+    component: Bar,
+    parameters: {
+        docs: {
+            page: mdx,
+        },
+        chromatic: { delay: 300 },
+    },
+    argTypes: {
+        useCanvas,
+        width,
+        height,
+        theme,
+        color,
+        leftMargin: margin,
+        rightMargin: margin,
+        topMargin: margin,
+        bottomMargin: margin,
+    },
+};
+
+const BarTemplate = (args) => (
+    <Chart
+        data={sales_records_dataset}
+        margin={{ left: args.leftMargin, right: args.rightMargin, top: args.topMargin, bottom: args.bottomMargin }}
+        ys={[args.y, args.y2, args.y3]}
+        xs={[args.x]}
+        {...args}
+    >
+        <YAxis fields={[args.y]} scaleType="band" showGridlines={false} />
+        <XAxis fields={[args.x, args.x2, args.x3]} />
+        <Bar x={args.x} y={args.y} color={args.color} animationDuration={args.animationDuration} />
+        {args.x2 ? (
+            <Bar x={args.x2} y={args.y} color={args.color2} animationDuration={args.animationDuration} />
+        ) : undefined}
+    </Chart>
+);
+
+const BarsTemplate = (args) => (
+    <Chart
+        data={sales_records_dataset}
+        margin={{ left: args.leftMargin, right: args.rightMargin, top: args.topMargin, bottom: args.bottomMargin }}
+        ys={[args.y, args.y2, args.y3]}
+        xs={[args.x]}
+        {...args}
+    >
+        <YAxis fields={[args.y]} scaleType="band" showGridlines={false} />
+        <XAxis fields={[args.x, args.x2, args.x3]} aggregate={args.stacked} />
+        <Bars
+            y={args.y}
+            xs={[args.x, args.x2]}
+            animationDuration={args.animationDuration}
+            grouped={args.grouped}
+            stacked={args.stacked}
+        />
+    </Chart>
+);
+
+export const Basic = BarTemplate.bind({});
+Basic.storyName = "Basic Plot";
+Basic.args = {
+    useCanvas: false,
+    width: 800,
+    height: 400,
+    animationDuration: 250,
+    color: "#99C1DC",
+    color2: "#FF7F28",
+    theme: "light",
+    leftMargin: 120,
+    rightMargin: 40,
+    topMargin: 40,
+    bottomMargin: 40,
+    onClick: console.debug,
+    onMouseOver: console.debug,
+    onMouseOut: console.debug,
+    x: "Unit Price",
+    y: "Item Type",
+};
+
+export const Color = BarTemplate.bind({});
+Color.storyName = "Custom Color";
+Color.args = {
+    ...Basic.args,
+    color: "orange",
+};
+
+export const Canvas = BarTemplate.bind({});
+Canvas.storyName = "Using Canvas";
+Canvas.args = {
+    ...Basic.args,
+    useCanvas: true,
+};
+
+export const Ratio = BarTemplate.bind({});
+Ratio.storyName = "Ratio Bars";
+Ratio.args = {
+    ...Basic.args,
+    x2: "Unit Cost",
+};
+
+export const Stacked = BarsTemplate.bind({});
+Stacked.storyName = "Stacked Bars";
+Stacked.args = {
+    ...Basic.args,
+    x2: "Unit Cost",
+    stacked: true,
+};
+
+export const Grouped = BarsTemplate.bind({});
+Grouped.storyName = "Grouped Bars";
+Grouped.args = {
+    ...Basic.args,
+    x2: "Unit Cost",
+    grouped: true,
+};
