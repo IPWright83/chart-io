443b913b875f7f34097d882ea98e3514e9e446d1
Ian Wright <ipwright83+dev@gmail.com>
2022-12-16 08:11:36 +0000

    Removed weird cruft from test

--- demos/react-demo/src/setupTests.js
+++ demos/react-demo/src/setupTests.js
@@ -2,4 +2,18 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
+
+// Polyfill crypto
+const nodeCrypto = require("crypto");
+
+window.crypto = {
+    getRandomValues: function (buffer) {
+        return nodeCrypto.randomFillSync(buffer);
+    },
+};
+
+// Dump debug messages to the void
+console.debug = () => {};
+console.warn = () => {};
+console.error = () => {};
