5ee044ff0886c061d2654cffaaf74c872633c4f0
Ian Wright <ipwright83+dev@gmail.com>
2023-03-15 19:15:53 +0000

    Added LegendItems

---
+++ packages/react/src/lib/components/LegendOverlay/Legend/LegendItem/LegendItem.tsx
@@ -0,0 +1,62 @@
+import type { ILegendItem } from "@d3-chart/types";
+import React from "react";
+import { useSelector } from "react-redux";
+
+import { getShape } from "../../../Shapes";
+import { chartSelectors, IState } from "../../../../store";
+
+export interface ILegendItemProps extends ILegendItem {
+    /**
+     * A completely custom format function, useful for translating to human friendly strings
+     * @param name    The key of the series value being formatted
+     * @param value   The value to format
+     */
+    format?: (name: string) => string;
+}
+
+/**
+ * Represents a row within a Legend
+ * @return {ReactElement}  The LegendItem component
+ */
+export function LegendItem({ name, icon, fill, format = (name) => name }: ILegendItemProps) {
+    const theme = useSelector((s: IState) => chartSelectors.theme(s));
+
+    const Shape = getShape(icon);
+
+    const styles = {
+        legendItem: {
+            display: "flex" as const,
+            flexDirection: "row" as const,
+            width: "100%",
+            userSelect: "none" as const,
+            pointerEvents: "none" as const,
+            color: theme.tooltip.text.toString(),
+            fontSize: theme.font.size,
+            fontFamily: theme.font.family,
+        },
+        legendValues: {
+            display: "flex" as const,
+            flexDirection: "row" as const,
+            justifyContent: "space-between" as const,
+            width: "100%",
+        },
+        legendSeriesName: {
+            marginRight: 15,
+            maxWidth: 215,
+            whiteSpace: "nowrap" as const,
+            overflow: "hidden" as const,
+            textOverflow: "ellipsis" as const,
+        },
+    };
+
+    return (
+        <div className="d3-chart legend-item" style={styles.legendItem}>
+            {Shape && <Shape fill={fill} />}
+            <div className="d3-chart legend-values" style={styles.legendValues}>
+                <span className="d3-chart legend-series-name" style={styles.legendSeriesName}>
+                    {format(name)}
+                </span>
+            </div>
+        </div>
+    );
+}
