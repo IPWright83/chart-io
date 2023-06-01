3ba8218299c095c9916fd3c133d7cb68927684ee
Ian Wright <ipwright83+dev@gmail.com>
2023-06-01 11:47:09 +0100

    Fixed unit test helper

--- packages/react/src/lib/testUtils/renderChart.tsx
+++ packages/react/src/lib/testUtils/renderChart.tsx
@@ -11,7 +11,7 @@ import { themes } from "../themes";
 export async function renderChart({
     children,
     data,
-    scales,
+    scales = {},
     store,
 }: {
     children: JSX.Element;
@@ -27,7 +27,15 @@ export async function renderChart({
                 width: 200,
             },
             data,
-            scales,
+            scales: Object.entries(scales).reduce((result, [key, scaleFunc]) => {
+                result[key] = {
+                    domain: scaleFunc.domain(),
+                    range: scaleFunc.range(),
+                    scale: scaleFunc,
+                };
+
+                return result;
+            }, {}),
             theme: themes.light,
         },
     });
