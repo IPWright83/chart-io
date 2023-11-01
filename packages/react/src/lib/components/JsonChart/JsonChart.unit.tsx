c92d5ffb12fef34a9246fd8fa26b1b1c65b674c7
Ian Wright <ipwright83+dev@gmail.com>
2023-11-01 11:13:14 +0000

    Added tests for the JsonChart

---
+++ packages/react/src/lib/components/JsonChart/JsonChart.unit.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { render } from "@testing-library/react";
+
+import { sales_records_dataset } from "../../../data/sales_records_dataset";
+
+import { JsonChart } from "./JsonChart";
+
+describe("JsonChart", () => {
+    it("should create empty template correctly", () => {
+        const { asFragment } = render(
+            <JsonChart
+                data={sales_records_dataset}
+                config={{
+                    chart: {
+                        id: "0000",
+                        width: 800,
+                    },
+                    axis: {
+                        x: {
+                            fields: "Order Date",
+                        },
+                    },
+                    series: {
+                        scatters: {
+                            ys: ["Total Cost", "Total Profit"],
+                            radius: 10,
+                        },
+                    },
+                }}
+            />,
+        );
+
+        expect(asFragment()).toMatchSnapshot();
+    });
+});
