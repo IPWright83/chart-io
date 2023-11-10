c933e527d85339e61d04045b2c1074445c95b919
Ian Wright <ipwright83+dev@gmail.com>
2023-11-10 12:08:33 +0000

    Removed a stray console log

--- packages/react/src/lib/components/Furniture/Lines/HorizontalLine/HorizontalLine.tsx
+++ packages/react/src/lib/components/Furniture/Lines/HorizontalLine/HorizontalLine.tsx
@@ -38,7 +38,6 @@ export function HorizontalLine({ y, value, opacity = 1, stroke }: IHorizontalLin
     }
 
     const yValue = scale(value as INumericValue);
-    console.log(scale, left, right, yValue);
 
     return (
         <line
