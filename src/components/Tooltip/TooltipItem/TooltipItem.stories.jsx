1728f295e92f2702ccc08916bddaada0f215aa2a
Ian Wright <ipwright83+dev@gmail.com>
2026-01-13 19:13:39 +0000

    Added tooltip item stories

--- src/components/Tooltip/TooltipItem/TooltipItem.stories.jsx
+++ src/components/Tooltip/TooltipItem/TooltipItem.stories.jsx
@@ -17,3 +17,35 @@ Scatter.args = {
     color: "steelblue",
     seriesType: "scatter",
 };
+
+export const Line = TooltipItemTemplate.bind({});
+Line.args = {
+    name: "Line Series",
+    value: 5,
+    color: "steelblue",
+    seriesType: "line",
+};
+
+export const Area = TooltipItemTemplate.bind({});
+Area.args = {
+    name: "Area Series",
+    value: 5,
+    color: "steelblue",
+    seriesType: "area",
+};
+
+export const Bar = TooltipItemTemplate.bind({});
+Bar.args = {
+    name: "Bar Series",
+    value: 5,
+    color: "steelblue",
+    seriesType: "bar",
+};
+
+export const Column = TooltipItemTemplate.bind({});
+Column.args = {
+    name: "Column Series",
+    value: 5,
+    color: "steelblue",
+    seriesType: "column",
+};
