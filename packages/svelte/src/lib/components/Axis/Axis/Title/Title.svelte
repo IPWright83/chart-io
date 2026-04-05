0a9b41ecd2ed04450b71a4361abcd4274a4c1073
Ian Wright <ipwright83+dev@gmail.com>
2026-04-05 22:19:29 +0100

    Add Title component

--- packages/svelte/src/lib/components/Axis/Axis/Title/Title.svelte
+++ packages/svelte/src/lib/components/Axis/Axis/Title/Title.svelte
@@ -1,21 +1,22 @@
 <script lang="ts">
-    import type { ITitleProps, IState } from "@chart-io/types";
-    import { chartSelectors } from "@chart-io/core";
+    import { chartSelectors, type IState } from "@chart-io/core";
+    import type { IPosition } from "@chart-io/types";
     import { useSelector } from "../../../../redux";
     import { getTransform } from "./getTransform";
+
+    export let position: IPosition;
+    export let title: string | undefined = undefined;
 
     let plotWidth = useSelector((s: IState) => chartSelectors.dimensions.plot.width(s));
     let plotHeight = useSelector((s: IState) => chartSelectors.dimensions.plot.height(s));
     let plotMargin = useSelector((s: IState) => chartSelectors.dimensions.plot.margin(s));
 
-    let transform = $derived(getTransform(position, plotWidth, plotHeight, plotMargin));
-
-    let { position, title }: ITitleProps = $props();
+    $: transform = getTransform(position, $plotWidth, $plotHeight, $plotMargin);
 </script>
 
 {#if title}
-    <text class={`chart-io axis-title axis-title-${position}`} transform={transform}>
-        {title} {$plotWidth} {$plotHeight}
+    <text class={`chart-io axis-title axis-title-${position}`} {transform}>
+        {title}
     </text>
 {/if}
 
