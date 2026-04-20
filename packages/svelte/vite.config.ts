f7ba1334c4f1ca3036b29c6390706e9993c37260
Ian Wright <ipwright83+dev@gmail.com>
2026-04-20 20:17:16 +0100

    Add svelte package setup

--- packages/svelte/vite.config.ts
+++ packages/svelte/vite.config.ts
@@ -1,6 +1,6 @@
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 import path from 'node:path';
 import { defineConfig } from 'vitest/config';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
 	plugins: [svelte()],
