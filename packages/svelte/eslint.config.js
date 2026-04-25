a1098d9859a7ac144cd2f21e2937fdac3ea3a4ad
Ian Wright <ipwright83+dev@gmail.com>
2026-04-25 09:28:29 +0100

    Remove prettier

--- packages/svelte/eslint.config.js
+++ packages/svelte/eslint.config.js
@@ -1,6 +1,5 @@
-import prettier from 'eslint-config-prettier';
-import js from '@eslint/js';
 import { includeIgnoreFile } from '@eslint/compat';
+import js from '@eslint/js';
 import svelte from 'eslint-plugin-svelte';
 import globals from 'globals';
 import { fileURLToPath } from 'node:url';
@@ -12,8 +11,6 @@ export default ts.config(
 	js.configs.recommended,
 	...ts.configs.recommended,
 	...svelte.configs['flat/recommended'],
-	prettier,
-	...svelte.configs['flat/prettier'],
 	{
 		languageOptions: {
 			globals: {
